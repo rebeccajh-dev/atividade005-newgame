e9526e9854d29e876e41cedd1879400492aa73ff
nathalia-nobrega <ttnast05@gmail.com>
2024-10-05 17:13:13 -0300

    refactor: modularizing files

---
+++ assets.py
@@ -0,0 +1,57 @@
+import pygame as pg
+from config import SCREEN_WIDTH, BASE_PLAYER_SIZE, P2_TITLE_OFFSET
+
+def load_assets():
+    # Font configuration
+    TEXT_FONT = pg.font.Font("assets/retro_font.ttf", 24)
+    NORMAL_FONT = pg.font.Font("assets/retro_font.ttf", 40)
+    TITLE_FONT = pg.font.Font("assets/retro_font.ttf", 72)
+
+    # Image configuration
+    TITLE_SPRITE = pg.image.load('assets/player_sprites/1/body.png')
+    TITLE_SPRITE = pg.transform.scale(TITLE_SPRITE, BASE_PLAYER_SIZE)
+    TITLE_SPRITE_RECT = TITLE_SPRITE.get_rect()
+    TITLE_SPRITE_RECT.center = (SCREEN_WIDTH // 2, 440)
+
+    TITLE_SPRITE2 = pg.image.load('assets/player_sprites/2/body.png')
+    TITLE_SPRITE2 = pg.transform.scale(TITLE_SPRITE2, BASE_PLAYER_SIZE)
+    TITLE_SPRITE_RECT2 = TITLE_SPRITE2.get_rect()
+    TITLE_SPRITE_RECT2.center = (SCREEN_WIDTH // 2, 440 + P2_TITLE_OFFSET)
+
+    TITLE_SPRITE_EYES = pg.image.load('assets/player_sprites/1/eyes.png')
+    TITLE_SPRITE_EYES = pg.transform.scale(TITLE_SPRITE_EYES, BASE_PLAYER_SIZE)
+    TITLE_SPRITE_EYES_RECT = TITLE_SPRITE_EYES.get_rect()
+    TITLE_SPRITE_EYES_RECT.center = ((SCREEN_WIDTH // 2) - 4, 440)
+
+    TITLE_SPRITE_EYES2 = pg.image.load('assets/player_sprites/1/eyes.png')
+    TITLE_SPRITE_EYES2 = pg.transform.scale(TITLE_SPRITE_EYES2, BASE_PLAYER_SIZE)
+    TITLE_SPRITE_EYES_RECT2 = TITLE_SPRITE_EYES2.get_rect()
+    TITLE_SPRITE_EYES_RECT2.center = ((SCREEN_WIDTH // 2) - 4, 440 + P2_TITLE_OFFSET)
+
+    UFO_SPRITE = pg.image.load('assets/UFO.png')
+    UFO_SPRITE = pg.transform.scale(UFO_SPRITE, (150, 150))
+    UFO_SPRITE_RECT = UFO_SPRITE.get_rect()
+    UFO_SPRITE_RECT.center = (SCREEN_WIDTH / 2, 420)
+
+    UFO_SPRITE2 = pg.image.load('assets/UFO2.png')
+    UFO_SPRITE2 = pg.transform.scale(UFO_SPRITE2, (150, 150))
+    UFO_SPRITE_RECT2 = UFO_SPRITE2.get_rect()
+    UFO_SPRITE_RECT2.center = (SCREEN_WIDTH / 2, 420 + P2_TITLE_OFFSET)
+
+    return {
+        "TEXT_FONT": TEXT_FONT,
+        "NORMAL_FONT": NORMAL_FONT,
+        "TITLE_FONT": TITLE_FONT,
+        "TITLE_SPRITE": TITLE_SPRITE,
+        "TITLE_SPRITE_RECT": TITLE_SPRITE_RECT,
+        "TITLE_SPRITE2": TITLE_SPRITE2,
+        "TITLE_SPRITE_RECT2": TITLE_SPRITE_RECT2,
+        "TITLE_SPRITE_EYES": TITLE_SPRITE_EYES,
+        "TITLE_SPRITE_EYES_RECT": TITLE_SPRITE_EYES_RECT,
+        "TITLE_SPRITE_EYES2": TITLE_SPRITE_EYES2,
+        "TITLE_SPRITE_EYES_RECT2": TITLE_SPRITE_EYES_RECT2,
+        "UFO_SPRITE": UFO_SPRITE,
+        "UFO_SPRITE_RECT": UFO_SPRITE_RECT,
+        "UFO_SPRITE2": UFO_SPRITE2,
+        "UFO_SPRITE_RECT2": UFO_SPRITE_RECT2
+    }
---
+++ config.py
@@ -0,0 +1,41 @@
+# Screen configuration
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 620
+FRAMERATE = 60
+
+# Basic color configuration
+PLAYER_COLORS = [(120, 255, 120), (200, 120, 200)]
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+COLOR_RED = (255, 0, 0)
+COLOR_DAMAGED = (160, 0, 0)
+UFO_COLORS = [
+    (240, 240, 240),
+    (240, 240, 240),
+    (240, 240, 240),
+    (100, 200, 100),
+    (100, 200, 100)
+]
+
+TERRAIN_COLORS = [
+    (0, 0, 0),
+    (40, 110, 60),
+    (40, 110, 60),
+    (80, 60, 40),
+    (80, 60, 40)
+]
+
+# Constant variables
+BASE_PLAYER_SIZE = (50, 50)
+BASE_SHIELD_X = 130
+BASE_SHIELD_Y = 20
+SHIELD_DISTANCE = 70
+ROUND_TIME = 180
+
+BASE_PLAYER_SPEED = 4
+BASE_ENEMY_SPEED = 2
+MAX_BULLET_SPEED = 5
+RANDOM_MOVE = [-1, -0.5, 0.25, 0, 0.25, 0.5, 1]
+
+# Interface configuration
+P2_TITLE_OFFSET = 15
